59056112878bff71cc5cb311dd8e3048fe5c222a
Rakesh kumar <rakeshkumaryadvanshi@gmail.com>
2022-06-21 22:50:24 -0400

    Add project files.

---
+++ CirculateArray/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+Console.WriteLine(string.Join("->", Circlulate.CirculateArray(new int[] { 1, 2, 3, 5, 6 })));
+
+Console.WriteLine(string.Join("->", Circlulate.CirculateArray(new int[] { 5, 6 })));
+
+Console.WriteLine(string.Join("->", Circlulate.CirculateArray(new int[] { 6 })));
+
+
+public class Circlulate
+{
+
+    // assumption array wouldn't be null
+    public static int[] CirculateArray(int[] array)
+    {
+        if (array.Length < 2)
+        {
+            return array;
+        }
+        var tempFirstItem = array[0];
+
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[array.Length - 1] = tempFirstItem;
+        return array;
+
+
+    }
+
+}
+
+
+
+
